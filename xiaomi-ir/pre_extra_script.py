d7d2cbe72a5c08610a4044f85ab714ec023198aa
Florian Baumann <derflob@derflob.de>
2023-05-24 13:11:19 +0200

    refactor xiaomi ir into package

---
+++ xiaomi-ir/pre_extra_script.py
@@ -0,0 +1,6 @@
+from shutil import copyfile
+
+copyfile(
+    "../../../packages/xiaomi-ir/remote_transmitter_esp32.cpp",
+    "./src/esphome/components/remote_transmitter/remote_transmitter_esp32.cpp"
+)
