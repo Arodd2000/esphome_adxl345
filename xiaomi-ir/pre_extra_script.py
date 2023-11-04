2e27be62e92e7b24a89485849dd92ab91e2352e2
Alex Rodgers <alex@rodgeville.com>
2023-11-04 15:51:20 -0400

    Moved files and deleted unrelated

--- xiaomi-ir/pre_extra_script.py
+++
@@ -1,6 +0,0 @@
-from shutil import copyfile
-
-copyfile(
-    "../../../packages/xiaomi-ir/remote_transmitter_esp32.cpp",
-    "./src/esphome/components/remote_transmitter/remote_transmitter_esp32.cpp"
-)
